18f8206598a73f777c86c35c9348a81684b787cd
FelipeCortez <felipecortezfi@gmail.com>
2021-01-30 15:31:25 -0300

    Less magic numbers

--- src/timelines/core.clj
+++ src/timelines/core.clj
@@ -27,30 +27,30 @@
   (if year
     (let [vertical-margin 32
           x               (remap year-range [(hinc pad) (hinc w)] year)
-          y               (+ 150 (* vertical-margin (get year->events year 0)))]
+          y               (+ 8 (* vertical-margin (get year->events year 0)))]
       [[:circle {:fill "#DD0000" :cx x :cy y :r 4}]
        [:text {:transform (format "translate(%s, %s) rotate(45)" x (+ 10 y))}
         event]])
     (let [vertical-margin 32
           x1              (remap year-range [(hinc pad) (hinc w)] (:from years))
           x2              (remap year-range [(hinc pad) (hinc w)] (:to years))
-          y               (+ 150 (* vertical-margin (get year->events year 0)))]
+          y               (+ 8 (* vertical-margin (get year->events year 0)))]
       [[:circle {:fill "#DD0000" :cx x1 :cy y :r 4}]
        [:circle {:fill "#DD0000" :cx x2 :cy y :r 4}]
        [:rect {:fill "#DD0000" :x x1 :y (- y 2) :width (- x2 x1) :height 4}]
        [:text {:transform (format "translate(%s, %s) rotate(45)" x1 (+ 10 y))}
         event]])))
 
-(defn timeline [data]
-  (let [initial-year 1945
-        end-year     (t/int (t/year (t/now)))
+(defn timeline [events]
+  (let [initial-year (:year (first events))
+        end-year     (:year (last events))
         year-range   [initial-year end-year]
 
         pad 40
         w   1200
-        h   360]
+        h   300]
     (together
-     [:svg {:viewBox (str "0 0 " (+ pad w) " 360")
+     [:svg {:viewBox (str "0 0 " (+ pad w) " " h)
             :width   (str (+ pad w) "px")
             :height  (str h "px")
             :xlmns   "http://www.w3.org/2000/svg"}]
@@ -61,18 +61,20 @@
                    (if (strong year)
                      (conj coll
                            [:line {:x1     snap :x2 snap
-                                   :y1     0    :y2 300
+                                   :y1     0    :y2 (- h 60)
                                    :stroke "gray"}]
-                           [:text {:transform (format "translate(%s, 320) rotate(45)"
-                                                      (int x))}
+                           [:text {:transform   (format "translate(%s, %s)"
+                                                        (int x)
+                                                        (-  h 40))
+                                   :text-anchor "middle"}
                             year])
                      (conj coll
                            [:line {:x1     snap :x2 snap
-                                   :y1     0    :y2 300
+                                   :y1     0    :y2 (- h 60)
                                    :stroke "lightgray"}]))))
                []
                (range initial-year (inc end-year))))
-     (loop [svg [], events data, year->events {}]
+     (loop [svg [], events events, year->events {}]
        (if (seq events)
          (let [{:keys [year years]} (first events)]
            (recur (apply conj svg (event->svg year-range pad w h year->events (first events)))
@@ -89,7 +91,9 @@
   (str (:html5 doctype)
        (html [:html {:lang "en"}
               [:head
-               [:style (css [:svg {:font-family   "Triplicate T4c"
+               [:style (css [:body {:padding "42px 0"
+                                    :margin "0"}
+                             :svg {:font-family   "Triplicate T4c"
                                    :font-size     "12px"
                                    :letter-spacing "-0.75px"}])]]
               [:body (timeline d/events)]])))
